f183d4950664c7f0b61e02129c335235b5bf2f91
RafaelDoering <rafael.doering.soares@usp.br>
2021-08-08 17:58:26 -0300

    Lint index.js file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
-const apiRouter = require("./routers");
+const apiRouter = require('./routers');
 
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true});
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  app.listen(port, () => {
-    console.log(`API listening at http://localhost:${port}`);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  app.listen(PORT, () => {
+    console.log(`API listening at http://localhost:${PORT}`);
   });
 });
